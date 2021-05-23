bcd309f27ead670bdde7ddcb971348bb25a6707c
Pashamks <74491257+Pashamks@users.noreply.github.com>
2021-05-23 20:07:17 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Telegram.Bot;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types.Enums;
@@ -6,10 +6,15 @@ using Telegram.Bot.Types.ReplyMarkups;
 using Telegram.Bot.Types.InlineQueryResults;
 using System.IO;
 using System.Threading.Tasks;
+
 namespace Hangman
 {
     class Program
     {
+        static bool b_play = false, b_write = false, b_letter = false;
+
+        static string current_word = "", letter_word;
+
         static TelegramBotClient Bot;
         static void Main(string[] args)
         {
@@ -47,35 +52,65 @@ namespace Hangman
             switch (message.Text)
             {
                 case "/start":
-                    string text_start = "Commands:\n/play - start a new game\n/help - "+
+                    string text_start = "Commands:\n/play - start a new game\n/help - " +
                         "displays the list of available commands\n" +
                         "/letter - to guess a letter\n/word - to guess the whole word";
                     await Bot.SendTextMessageAsync(message.From.Id, text_start);
-                        break;
+                    break;
                 case "/play":
-                    string text_play = "Let's start! I have selected the word."+
-                        " So choose command /letter or /word to start guessing!" +
+                    string text_play = "Let's start! I have selected the word." +
+                        " So choose a command /letter or /word to start guessing!" +
                         " Your word is :\n" + Generate_word();
                     await Bot.SendTextMessageAsync(message.From.Id, text_play);
+                    b_play = true;
+                    b_write = false;
+                    b_letter = false;
                     break;
                 case "/help":
-                    string text_help = "In our game i'm going to select the word and you need to guess it."+
-                        " You can see how to play it in real life here.\n"+
+                    string text_help = "In our game i'm going to select the word and you need to guess it." +
+                        " You can see how to play it in real life here.\n" +
                         "https://www.wikihow.com/Play-Hangman";
                     await Bot.SendTextMessageAsync(message.From.Id, text_help);
                     break;
                 case "/letter":
                     string text_letter = "Okey, sent me a letter!";
                     await Bot.SendTextMessageAsync(message.From.Id, text_letter);
+                    b_write = false;
+                    b_letter = true;
                     break;
                 case "/word":
                     string text_word = "Okey, sent me a whole word!";
                     await Bot.SendTextMessageAsync(message.From.Id, text_word);
-
+                    b_write = true;
+                    b_letter = false;
                     break;
                 default:
-                    string text_def = "I don't know this command";
-                    await Bot.SendTextMessageAsync(message.From.Id, text_def);
+                    if (b_play)
+                    {
+                        if (b_write)
+                        {
+                            await Bot.SendTextMessageAsync(message.From.Id, Compare_word(current_word, message.Text));
+                        }
+                        else if (b_letter)
+                        {
+                            await Bot.SendTextMessageAsync(message.From.Id, Find_letter(message.Text[0]));
+                        }
+                        else
+                        {
+                            string text_p = "Choose the command /letter or /word to start guessing!";
+                            await Bot.SendTextMessageAsync(message.From.Id, text_p);
+                        }
+                    }
+                    else if (b_letter || b_write)
+                    {
+                        await Bot.SendTextMessageAsync(message.From.Id, "Sorry, I thought that you were gone... Please,"+
+                            "enter /play again");
+                    }
+                    else
+                    {
+                        string text_def = "I don't know this command";
+                        await Bot.SendTextMessageAsync(message.From.Id, text_def);
+                    }
                     break;
             }
             // read current word from file
@@ -100,14 +135,73 @@ namespace Hangman
             // generate line of _ 
             static string Generate_word(){
                 Random rand = new Random();
-                string val = Get_word(rand.Next(1,4));
-                Console.WriteLine(val);
+                current_word = Get_word(rand.Next(1,11));
+                
+                Console.WriteLine(current_word);
                 string result = "";
-                for(int i = 0; i < val.Length; ++i)
+                for(int i = 0; i < current_word.Length; ++i)
                 {
                     result += "_ ";
                 }
                 return result ;
+            }
+
+            static string Find_letter(char letter)
+            {
+                if(!current_word.Contains(letter))
+                return "I'm so sorry...There is no letter like that in my word.";
+
+                string letter_result = "Your word after guessing:\n", temp_word = "";
+                for(int i =0; i < current_word.Length; ++i)
+                {
+                    if (current_word[i] == letter)
+                    {
+                        temp_word += letter;
+                        temp_word += " ";
+                    }
+                    else
+                    {
+                            temp_word += "_ ";
+                    } 
+                }
+
+                if (letter_word == null)
+                {
+                    letter_word = temp_word;
+                }
+                    
+                else
+                {
+                    int i = 0;
+                    foreach(var ch in letter_word)
+                    {
+                        if (letter_word[i] == '_' || letter_word[i] == ' ')
+                        {
+                            letter_word = letter_word.Remove(i, 1);
+                            letter_word = letter_word.Insert(i, temp_word[i].ToString());
+                        }
+                        ++i;
+                    }
+                   
+                }
+                letter_result += letter_word;
+                
+                if(letter_word.Replace(" ", "") == current_word)
+                {
+                    return "Congratulation! You won the game ! To start a new game enter /play";
+                }
+
+                return letter_result;
+            }
+
+            static string Compare_word(string str1, string str2)
+            {
+                if(str1 == str2)
+                {
+                    return "Congratulation! You won the game ! To start a new game enter /play";
+                }
+                b_play = false;
+                return "I'm so sorry, but you lost...";
             }
         }
     }
